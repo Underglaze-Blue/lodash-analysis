76aba14540b30ed3e3be0733cc3434a9dd0c3d5b
Underglaze-Blue <lepapillonangel@gmail.com>
2021-01-14 22:25:57 +0800

    remade

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Underglaze-Blue/lodash-analysis.git main:gh-pages
+git push -f git@github.com:Underglaze-Blue/lodash-analysis.git master:gh-pages
 
 cd -
